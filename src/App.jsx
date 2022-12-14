ac3c85093fbcf00b9fd8ba34022b26ac3f109a98
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2022-12-14 12:17:33 -0300

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
       <img className="imageHero" src={imageHero} alt="image hero" />
-      <div className="containerData">
+      <main className="containerData">
         <h1 className="title">Order Summary</h1>
         <p className="text">
           You can now listen to millons of songs, audiobooks, and podcasts on
@@ -28,7 +28,7 @@ function App() {
         </div>
         <button className="btnPayment">Proceed to Payment</button>
         <button className="btnCancelOrder">Cancel Order</button>
-      </div>
+      </main>
     </div>
   );
 }
